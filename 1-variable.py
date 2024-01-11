ba79f320a0572c827b41a265e884508135c1cd7d
nekosama <lelong190110@gmail.com>
2024-01-11 22:28:37 +0700

    con meo con

---
+++ 1-variable.py
@@ -0,0 +1,20 @@
+
+
+
+chinchin = chuchu = chacha = "meomeo"
+
+print(chuchu)# meomeo
+print("concu") # concu
+print(chinchin) # meomeo
+aut = 5
+au = 4.3
+print(chinchin + str(aut)) #meomeo5 chuyển aut từ int sang str 
+print( type(au)) #<class 'float'>
+print(chinchin.lower()) # meomeo
+print(chinchin.upper()) #MEOMEO
+print(chinchin.isalpha()) # false or true 
+print(chinchin.count("o")) #2
+print(chinchin.replace("o","i")) #mei mei
+print(chinchin*3) # meomeomeomeomeomeo 3 times
+
+
---
+++ 25-string-char.py
@@ -0,0 +1,21 @@
+# x = 2
+# y = 3.0
+# z = "4"
+
+# print(x,y,z*2)
+
+# y = int(y)
+# z = int(z)
+# print(x,y,z*2)
+
+# 2 3.0 44
+# 2 3 8
+
+name = input("what is your name : ") #long
+age  = int(input("how old are u : "))#17 // phải ép thành int để cộng thêm 1 vì string không chấp nhận dấu cộng 
+age = age + 1
+print("con meo " + name) # con meo ngu  // chỉ cho input 1 từ thôi 
+print(age)
+
+
+
